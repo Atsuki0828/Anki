ab957e0d8913467acddb02f3afd3eb668b148a54
name <77559844+Atsuki0828@users.noreply.github.com>
2026-06-16 11:56:50 +0900

    重問2024準拠: 修正解答の英語名とラテン語名を更新

---
+++ terminology-corrections-heavy2024.js
@@ -0,0 +1,18 @@
+window.ANATOMY_TRANSLATIONS=Object.assign(window.ANATOMY_TRANSLATIONS||{}, {
+  95:{
+    en:['Rhomboid major muscle','Rhomboid minor muscle','Levator scapulae muscle','Pectoralis major muscle','Pectoralis minor muscle','Latissimus dorsi muscle'],
+    la:['Musculus rhomboideus major','Musculus rhomboideus minor','Musculus levator scapulae','Musculus pectoralis major','Musculus pectoralis minor','Musculus latissimus dorsi']
+  },
+  411:{
+    en:['Pubococcygeus muscle','Iliococcygeus muscle'],
+    la:['Musculus pubococcygeus','Musculus iliococcygeus']
+  },
+  425:{
+    en:['Pudendal nerve','Pelvic splanchnic nerves'],
+    la:['Nervus pudendus','Nervi splanchnici pelvici']
+  },
+  486:{
+    en:['Ascending pharyngeal artery','Inferior thyroid artery','Branches of lingual artery','Branches of maxillary artery'],
+    la:['Arteria pharyngea ascendens','Arteria thyroidea inferior','Rami arteriae lingualis','Rami arteriae maxillaris']
+  }
+});
