523e575af306ad1ee08d302d2228b77d48419e5f
name <77559844+Atsuki0828@users.noreply.github.com>
2026-06-17 17:56:38 +0900

    医学資料精査: 下肢作用筋の対訳を更新2

---
+++ terminology-corrections-medical-audit-2-lower-c.js
@@ -0,0 +1,7 @@
+window.ANATOMY_TRANSLATIONS=Object.assign(window.ANATOMY_TRANSLATIONS||{}, {
+220:{en:['Adductor magnus muscle','Adductor longus muscle','Adductor brevis muscle','Gracilis muscle','Pectineus muscle'],la:['Musculus adductor magnus','Musculus adductor longus','Musculus adductor brevis','Musculus gracilis','Musculus pectineus']},
+221:{en:['Anterior fibers of gluteus medius','Anterior fibers of gluteus minimus','Tensor fasciae latae muscle'],la:['Fibrae anteriores musculi glutei medii','Fibrae anteriores musculi glutei minimi','Musculus tensor fasciae latae']},
+223:{en:['Quadriceps femoris muscle'],la:['Musculus quadriceps femoris']},
+225:{en:['Biceps femoris muscle'],la:['Musculus biceps femoris']},
+227:{en:['Gastrocnemius muscle','Soleus muscle','Plantaris muscle','Tibialis posterior muscle','Flexor digitorum longus muscle','Flexor hallucis longus muscle','Fibularis longus muscle','Fibularis brevis muscle'],la:['Musculus gastrocnemius','Musculus soleus','Musculus plantaris','Musculus tibialis posterior','Musculus flexor digitorum longus','Musculus flexor hallucis longus','Musculus fibularis longus','Musculus fibularis brevis']}
+});
