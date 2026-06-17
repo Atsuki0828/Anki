335a3d956ce03a43ee9a5eea62114ebb467060ee
name <77559844+Atsuki0828@users.noreply.github.com>
2026-06-17 17:55:06 +0900

    医学資料精査: 下肢作用筋の対訳を更新1

---
+++ terminology-corrections-medical-audit-2-lower-b.js
@@ -0,0 +1,7 @@
+window.ANATOMY_TRANSLATIONS=Object.assign(window.ANATOMY_TRANSLATIONS||{}, {
+177:{en:['Inferomedial border: medial head of gastrocnemius','Inferolateral border: lateral head of gastrocnemius'],la:['Margo inferomedialis: caput mediale musculi gastrocnemii','Margo inferolateralis: caput laterale musculi gastrocnemii']},
+178:{en:['Tibial nerve','Common fibular nerve'],la:['Nervus tibialis','Nervus fibularis communis']},
+217:{en:['Iliopsoas muscle','Rectus femoris muscle','Tensor fasciae latae muscle','Anterior fibers of gluteus medius','Anterior fibers of gluteus minimus'],la:['Musculus iliopsoas','Musculus rectus femoris','Musculus tensor fasciae latae','Fibrae anteriores musculi glutei medii','Fibrae anteriores musculi glutei minimi']},
+218:{en:['Gluteus maximus muscle','Semitendinosus muscle','Semimembranosus muscle','Long head of biceps femoris','Hamstring part of adductor magnus'],la:['Musculus gluteus maximus','Musculus semitendinosus','Musculus semimembranosus','Caput longum musculi bicipitis femoris','Pars ischiocondylaris musculi adductoris magni']},
+219:{en:['Gluteus medius muscle','Gluteus minimus muscle','Tensor fasciae latae muscle'],la:['Musculus gluteus medius','Musculus gluteus minimus','Musculus tensor fasciae latae']}
+});
