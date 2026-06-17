21f9534f71f873cc0e2104fcbd92e85d664d886b
name <77559844+Atsuki0828@users.noreply.github.com>
2026-06-17 17:42:53 +0900

    医学資料精査: 下肢対訳を追加1

---
+++ terminology-corrections-medical-audit-2-lower-a.js
@@ -0,0 +1,8 @@
+window.ANATOMY_TRANSLATIONS=Object.assign(window.ANATOMY_TRANSLATIONS||{}, {
+118:{en:['Dorsal scapular nerve','Long thoracic nerve','Nerve to subclavius','Suprascapular nerve'],la:['Nervus dorsalis scapulae','Nervus thoracicus longus','Nervus subclavius','Nervus suprascapularis']},
+155:{en:['Inferior gluteal vessels','Internal pudendal vessels','Sciatic nerve','Posterior femoral cutaneous nerve','Inferior gluteal nerve','Pudendal nerve','Nerve to obturator internus','Nerve to quadratus femoris'],la:['Vasa glutea inferiora','Vasa pudenda interna','Nervus ischiadicus','Nervus cutaneus femoris posterior','Nervus gluteus inferior','Nervus pudendus','Nervus musculi obturatorii interni','Nervus musculi quadrati femoris']},
+167:{en:['Superior: inguinal ligament','Lateral: medial border of sartorius muscle','Medial: medial border of adductor longus muscle'],la:['Superior: ligamentum inguinale','Lateral: margo medialis musculi sartorii','Medial: margo medialis musculi adductoris longi']},
+171:{en:['Long head: tibial division of sciatic nerve','Short head: common fibular division of sciatic nerve'],la:['Caput longum: divisio tibialis nervi ischiadici','Caput breve: divisio fibularis communis nervi ischiadici']},
+172:{en:['Tibial division of sciatic nerve'],la:['Divisio tibialis nervi ischiadici']},
+173:{en:['Tibial division of sciatic nerve'],la:['Divisio tibialis nervi ischiadici']}
+});
