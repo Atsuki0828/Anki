e0ee1107133c2569807d4183d1f40ce3a6605ad5
name <77559844+Atsuki0828@users.noreply.github.com>
2026-06-17 17:19:04 +0900

    医学資料精査: 上肢対訳を追加

---
+++ terminology-corrections-medical-audit-2-upper-a.js
@@ -0,0 +1,7 @@
+window.ANATOMY_TRANSLATIONS=Object.assign(window.ANATOMY_TRANSLATIONS||{}, {
+5:{en:['Coracohumeral ligament','Superior glenohumeral ligament','Middle glenohumeral ligament','Inferior glenohumeral ligament'],la:['Ligamentum coracohumerale','Ligamentum glenohumerale superius','Ligamentum glenohumerale medium','Ligamentum glenohumerale inferius']},
+26:{en:['Pectoralis major muscle','Pectoralis minor muscle','Subclavius muscle'],la:['Musculus pectoralis major','Musculus pectoralis minor','Musculus subclavius']},
+29:{en:['Intertubercular sulcus of humerus'],la:['Sulcus intertubercularis humeri']},
+34:{en:['Biceps brachii muscle','Brachialis muscle','Coracobrachialis muscle'],la:['Musculus biceps brachii','Musculus brachialis','Musculus coracobrachialis']},
+42:{en:['Pronator teres muscle','Flexor carpi radialis muscle','Palmaris longus muscle','Flexor carpi ulnaris muscle'],la:['Musculus pronator teres','Musculus flexor carpi radialis','Musculus palmaris longus','Musculus flexor carpi ulnaris']}
+});
